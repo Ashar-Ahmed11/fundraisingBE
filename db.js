14ef1953707388b7f420c429ce1cce9df31abae5
Ashar <ashar.a.hmd11@gmail.com>
2025-06-11 19:41:00 +0500

    anotherchange

--- db.js
+++ db.js
@@ -1,9 +1,16 @@
 const mongoose = require('mongoose')
+const mongoURI = "mongodb+srv://akhuwat:UIAZGDS@akhuwat-database.l9ey8xt.mongodb.net/?retryWrites=true&w=majority&appName=akhuwat-database"
 
-const URI = 'mongodb+srv://akhuwat:UIAZGDS@akhuwat-database.l9ey8xt.mongodb.net/?retryWrites=true&w=majority&appName=akhuwat-database'
-mongoose.set("strictQuery", false);
-const connectToMongoose = () => mongoose.connect(URI, () => {
-    console.log("Connected to Mongo Successfully")
-})
+const connectToMongoose = async () => {
+  try {
+    await mongoose.connect(mongoURI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+    console.log('Connected to Mongoose successfully');
+  } catch (error) {
+    console.error('Error connecting to Mongoose:', error.message);
+  }
+};
 
-module.exports = connectToMongoose
+module.exports = connectToMongoose;
